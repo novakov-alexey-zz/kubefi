c6b713c7c2ff3999de02e8e1d66e06f94ace81ec
Alexey Novakov <novakov.alex@gmail.com>
2020-08-02 21:54:25 +0200

    extract version

--- kubefi-deployments/build-image.sh
+++ kubefi-deployments/build-image.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-ver=$1
+# build musl binary via docker
 rm -r target
-echo "version $ver"
 alias rust-musl-builder='docker run --rm -it  -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder:stable'
 rust-musl-builder cargo build --release
 
-# prepare content
+# prepare content for the app image
 mkdir target/docker
 cp -r templates/ target/docker/templates
 cp docker/Dockerfile target/docker
@@ -13,4 +12,7 @@ cp target/x86_64-unknown-linux-musl/release/kubefi-deployments target/docker/
 mkdir -p target/docker/conf/
 cp -r conf/ target/docker/conf
 
+# build the app image
+ver=$1
+echo "version: $ver"
 docker build -t kubefi-deployments-operator:${ver} -f target/docker/Dockerfile target/docker/
